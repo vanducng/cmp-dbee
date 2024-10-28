45480e3abc80ab025044aae6841fe4ee8f331faa
MattiasMTS <mattias.sjodin@northvolt.com>
2024-10-28 20:33:15 +0100

    docs: add types to database folder

--- lua/cmp-dbee/database/init.lua
+++ lua/cmp-dbee/database/init.lua
@@ -1,13 +1,27 @@
+---@mod cmp-dbee.database Database Module
+---@brief [[
+---This module provides functions to interact with the database, including
+---fetching the database structure, models, and column completions.
+---
+---Access it like this:
+--->
+---local Database = require("cmp-dbee.database")
+---<
+---@brief ]]
+
 local dbee_core = require("dbee.api.core")
 local dbee_ui = require("dbee.api.ui")
 
+--- @class Database
 local Database = {}
 Database.cache = {} -- Cache for storing the current database structure
 Database.column_cache = {} -- Cache for storing columns of specific tables
 Database.cache_expiry_s = 10 -- seconds
 Database.cache_last_updated = {}
 
-Database.get_current_connection = function()
+--- Get the current connection ID.
+--- @return ConnectionID|nil connection_id The current connection ID or nil if not available.
+function Database.get_current_connection()
   local ok, connection_id = pcall(dbee_core.get_current_connection)
   if not ok then
     return nil
@@ -15,12 +29,16 @@ Database.get_current_connection = function()
   return connection_id
 end
 
-Database.is_available = function()
+--- Check if the database is available (if dbee core and ui loaded)
+--- @return boolean True if the database is available, false otherwise.
+function Database.is_available()
   return dbee_core.is_loaded() and dbee_ui.is_loaded()
 end
 
--- Function to get the current database structure
-Database.get_db_structure = function(callback)
+--- Get the current database structure. The structure
+--- is of type DBStructure[].
+--- @param callback Callback Callback function to return the database structure (cached or not).
+function Database.get_db_structure(callback)
   local connection_id = Database.get_current_connection()
   if not connection_id then
     callback {}
@@ -30,13 +48,11 @@ Database.get_db_structure = function(callback)
     Database.cache[connection_id.id]
     and os.time() - (Database.cache_last_updated[connection_id.id] or 0) < Database.cache_expiry_s
   then
-    -- TODO: print("Using cached structure")
     callback(Database.cache[connection_id.id])
     return
   end
 
-  -- TODO: print("Fetching structure")
-  -- Fetch and cache the structure asynchronously
+  -- async fetch and cache
   vim.defer_fn(function()
     local structure = dbee_core.connection_get_structure(connection_id.id)
     Database.cache[connection_id.id] = structure
@@ -45,7 +61,10 @@ Database.get_db_structure = function(callback)
   end, 0)
 end
 
-Database.get_models = function(schema, callback)
+--- Get the models for a specific schema.
+--- @param schema string The schema name.
+--- @param callback Callback Callback function to return the models.
+function Database.get_models(schema, callback)
   local connection_id = Database.get_current_connection()
   if not connection_id then
     callback {}
@@ -71,15 +90,17 @@ Database.get_models = function(schema, callback)
   end)
 end
 
--- Function to get column completions for a specific schema and model
-Database.get_column_completion = function(schema, model, callback)
+--- Get column completions for a specific schema and model.
+--- @param schema string The schema name.
+--- @param model string The model name.
+--- @param callback Callback Callback function to return the columns.
+function Database.get_column_completion(schema, model, callback)
   local connection_id = dbee_core.get_current_connection()
   if not connection_id then
     callback {}
     return
   end
 
-  -- Check if columns for the specified model are cached
   if
     Database.column_cache[connection_id.id]
     and Database.column_cache[connection_id.id][schema]
@@ -113,7 +134,7 @@ Database.get_column_completion = function(schema, model, callback)
   end, 0)
 end
 
--- Function to manually clear the cache for the current connection
+--- Clear the cache for the current connection.
 local function clear_cache()
   local connection_id = dbee_core.get_current_connection()
   if connection_id then
@@ -125,4 +146,5 @@ end
 -- Register event listeners to invalidate the cache
 dbee_core.register_event_listener("current_connection_changed", clear_cache)
 dbee_core.register_event_listener("database_selected", clear_cache)
+
 return Database
